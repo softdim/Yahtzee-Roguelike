36c0281cc92cb2c05ca912edf1dcfa4c5e80e558
david <dhollisalt@gmail.com>
2025-10-03 09:33:58 -0500

    Updated yahtzee.py
    Strung together the game to function at basic capacity.

--- yahtzee.py
+++ yahtzee.py
@@ -3,25 +3,21 @@ from patterns import *
 
 # GameState holds all of the data for the current game
 class GameState:
-    # ndice = number of dice
-    # patterns = patterns that can be claimed as the game progresses
     def __init__(self, ndice=5, patterns = PATTERNS):
         self.ndice = ndice
         self.dice = [Die(self) for _ in range(ndice)]
         self.patterns = patterns
         self.score = 0
     
-    # rolls all the dice that are not currently held
+
     def roll(self):
         for die in self.dice:
             if not die.held:
                 die.roll()
     
-    # run every time after the player takes an action
     def update(self):
         self.roll()
 
-    # gs.values = [dice1.value, ...]
     @property
     def values(self):
         return [d.value for d in self.dice]
